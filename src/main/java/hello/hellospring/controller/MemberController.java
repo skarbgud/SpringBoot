02d04537a03ee687d70970bbaa54c7c43bc13994
Nam <sjarbgud@gmail.com>
2020-08-08 02:13:22 +0900

    Member - create

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 컨트롤러가 스프링 빈으로 자동등록된 이유도 컴포넌트 스캔 때문이다.
 public class MemberController {
@@ -25,4 +28,19 @@ public class MemberController {
 //        this.memberService = memberService;
 //    }
     // 거의 생성자 주입 사용
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
